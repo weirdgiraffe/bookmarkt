7b065d66db9f186ae6516ccd6dabe6c737fa5073
vlnk <vlnk@protonmail.com>
2020-09-12 00:33:37 -0400

    ✨ Read an input file with kuchiki

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use kuchiki::parse_html;
+use kuchiki::traits::TendrilSink;
+use kuchiki::NodeRef;
+
+use std::io;
+use std::path::Path;
+
+#[derive(Debug)]
+pub struct BookmarkImport {
+    pub dom: NodeRef,
+}
+
+impl BookmarkImport {
+    pub fn new(handle: NodeRef) -> Self {
+        BookmarkImport { dom: handle }
+    }
+
+    pub fn from_file(path: &Path) -> Result<Self, io::Error> {
+        parse_html()
+            .from_utf8()
+            .from_file(path)
+            .and_then(|dom| Ok(Self::new(dom)))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "{:?}",
+        BookmarkImport::from_file(Path::new("./res/sample.html"))
+    );
 }
