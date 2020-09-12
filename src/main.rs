563d48d8e5dbe4f3d9c3bdcb363b72ebc2de2157
vlnk <vlnk@protonmail.com>
2020-09-12 19:19:41 -0400

    ✅ Add unit test that loads a file with kuchiki

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,46 @@ use kuchiki::parse_html;
 use kuchiki::traits::TendrilSink;
 use kuchiki::NodeRef;
 
-use std::io;
+use std::fs;
+use std::io::Error;
 use std::path::Path;
 
 #[derive(Debug)]
-pub struct BookmarkImport {
+pub struct Bookmarkt {
     pub dom: NodeRef,
 }
 
-impl BookmarkImport {
+impl Bookmarkt {
     pub fn new(handle: NodeRef) -> Self {
-        BookmarkImport { dom: handle }
+        Bookmarkt { dom: handle }
     }
 
-    pub fn from_file(path: &Path) -> NodeRef {
-        parse_html().from_utf8().from_file(path).unwrap()
+    pub fn from_file(path: &Path) -> Result<Self, Error> {
+        parse_html()
+            .from_utf8()
+            .from_file(path)
+            .and_then(|dom| Ok(Bookmarkt::new(dom)))
+    }
+
+    pub fn to_string(&self) -> String {
+        self.dom.to_string()
     }
 }
 
+const NETSCAPE_FILE: &str = "./res/netscape.html";
+
 fn main() {
-    println!(
-        "{:?}",
-        BookmarkImport::from_file(Path::new("./res/netscape.html")).to_string()
-    );
+    let path = Path::new(NETSCAPE_FILE);
+    let bookmarkt = Bookmarkt::from_file(path).unwrap();
+    println!("{:?}", bookmarkt.to_string());
+
+    let contents = fs::read_to_string(path).unwrap();
+    println!("{:?}", contents)
+}
+
+#[test]
+fn load_file_with_kuchiki() {
+    let path = Path::new(NETSCAPE_FILE);
+    let bookmarkt = Bookmarkt::from_file(path).unwrap();
+    assert_eq!(bookmarkt.to_string().is_empty(), false);
 }
