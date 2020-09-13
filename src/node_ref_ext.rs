56204d0ce227c80a2ab35505db57ffc938ac27d8
vlnk <vlnk@protonmail.com>
2020-09-13 16:04:39 -0400

    ✅ Check if NodeRefExt takes care of the selector case

--- src/node_ref_ext.rs
+++ src/node_ref_ext.rs
@@ -21,3 +21,22 @@ impl NodeRefExt for NodeRef {
         content
     }
 }
+
+#[test]
+fn ignore_selector_case() {
+    use kuchiki::parse_html;
+    use kuchiki::traits::TendrilSink;
+
+    let selector = "TITLE";
+
+    let upper = r"<TITLE>Test</Title>";
+    let capital = r"<Title>Test</Title>";
+    let lower = r"<title>Test</title>";
+
+    let from_upper = parse_html().one(upper).select_text(selector).unwrap();
+    let from_capital = parse_html().one(capital).select_text(selector).unwrap();
+    let from_lower = parse_html().one(lower).select_text(selector).unwrap();
+
+    assert_eq!(from_upper, from_capital);
+    assert_eq!(from_capital, from_lower);
+}
